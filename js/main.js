201be526fca8a82173765cda0ade4c82a7fa98a1
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-06-20 16:18:27 -0500

    feat: :sparkles: Busqueda.
    Funciones que permiten que se muestre las coincidencias que va ingresando el usuario.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 import { getAllData } from "./module/api.js"; //Importa la funcion para traer la data. 
 import { allUsersHTML } from "./components/users.js"; //Importa la funcion que devuelve la info del HTML.
+import { userInfoById } from "./module/userInfo.js"; // Importa la funcion que devuelve el dic
+import { filtrarDicts } from "./module/nombreDescripcion.js"; //Importa la funcion que filtra los diccionarios con la info brindada.
 
 let main = document.querySelector(".main"); //Trae la etiqueta main del HTML
-main.innerHTML = await allUsersHTML(await getAllData()); // Envia la data a la funcion allUsersHTML y retorna la plantila para agregarla al main. 
+let inputSearch = document.querySelector("#inputSearch");// Trae la etiqueta input del HTML
+
+
+
+document.addEventListener("DOMContentLoaded", async e=>{ //Cada vez que ingrese un nuevo dato se actualiza.
+    
+    main.innerHTML = await allUsersHTML(await getAllData()); // Envia la data a la funcion allUsersHTML y retorna la plantila para agregarla al main. 
+    
+    inputSearch.addEventListener('input', async e => { //Cada vez que ingrese un nuevo dato se actualiza.
+        let infoIngresada = (e.target.value).toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "");//La variable infoIngresada guarda la info del buscador sin espacios a los extremos, sin tildes y en minusculas. 
+        main.innerHTML = await allUsersHTML(await filtrarDicts(await getAllData(), infoIngresada)); //Envia la data a la funcion filtrarDicts y luego a la funcion allUsersHTML para que retorne la plantila para agregarla al main.
+    });
+});
---
+++ js/module/userInfo.js
@@ -0,0 +1,6 @@
+export const userInfoById = async dict => {
+    let {id} = dict;
+    let res = await fetch(`https://6674179975872d0e0a950e53.mockapi.io/user/${id}`);
+    let data = res.json();
+    return data;
+};
